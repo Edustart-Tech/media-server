f8d66cff805251caefc5b2ba1e776c1b846845c4
timizuoebideri <timizuoebideri@gmail.com>
2025-03-27 00:03:31 +0100

    add filtering multi choice to MediaFileFilter.html_index_path

--- media_library/filterset.py
+++ media_library/filterset.py
@@ -5,11 +5,19 @@ from media_library.models import MediaFile
 
 class MediaFileFilter(rest_framework.FilterSet):
     file = rest_framework.CharFilter(field_name='file', lookup_expr='iexact')
+    html_index_path = rest_framework.CharFilter(method='filter_html_index_path')
+
     class Meta:
         model = MediaFile
         fields = (
             "is_html",
-            "html_index_path",
             "html_base_dir",
             "file_type",
         )
+
+    def filter_html_index_path(self, queryset, name, value):
+        # This was the only way I could make html_index_path to accept multiple entry filtering
+        value = self.data.getlist("html_index_path")
+        if value:
+            queryset = queryset.filter(html_index_path__in=value)
+        return queryset
